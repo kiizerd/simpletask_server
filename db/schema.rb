c84c20347bfc007890b183cf7b57e04b86abf32d
kiizerd <ajsewell13@gmail.com>
2023-05-31 22:06:13 -0500

    Added user_id reference column to Project model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_19_011431) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_24_035900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_19_011431) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
+    t.bigint "user_id", default: 7, null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "sections", force: :cascade do |t|
@@ -55,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_19_011431) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "projects", "users"
   add_foreign_key "sections", "projects"
   add_foreign_key "tasks", "projects"
   add_foreign_key "tasks", "sections"
