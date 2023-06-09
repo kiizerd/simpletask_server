18e5a3a62c54fb5dcee182406b50dfcd8c0ff97c
kiizerd <ajsewell13@gmail.com>
2023-06-09 12:27:13 -0500

    Added production client URL to CORS list.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # Vite dev server at 5173 and production at 8000
-    origins 'http://localhost:5173', 'http://localhost:8000'
+    # Vite dev server at 5173, preview at 8000, and production url
+    origins 'http://localhost:5173', 'http://localhost:8000', 'https://simpletask.onrender.com'
 
     resource '*',
              headers: :any,
