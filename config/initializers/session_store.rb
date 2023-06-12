93a985002eae566f49ec938ee3967629e26d6f65
kiizerd <ajsewell13@gmail.com>
2023-06-11 19:10:42 -0500

    Set secure true.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: 'token', domain: :all, same_site: :none
+Rails.application.config.session_store :cookie_store, key: 'token', domain: :all, same_site: :none, secure: true
