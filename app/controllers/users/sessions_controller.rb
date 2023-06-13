a985e268edf6eea50f4ab2245ae5f5716e47fde3
kiizerd <ajsewell13@gmail.com>
2023-06-12 21:47:38 -0500

    Improved error handling, added action to get user.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  skip_before_action :authenticate_user!, only: :create
 
-  skip_before_action :authenticate_user!, only: %i[create destroy]
+  def show
+    render json: { user: @current_user }, status: :ok
+  end
 
   # POST /resource/sign_in
   def create
@@ -12,8 +14,7 @@ class Users::SessionsController < Devise::SessionsController
       generate_session_cookie(user)
       respond_to_create(user)
     else
-      errors = user.nil? ? 'Email not registered' : user.errors.messages
-      render json: { errors: }, status: :unauthorized
+      render status: user ? :unauthorized : :not_found
     end
   end
 
@@ -21,7 +22,7 @@ class Users::SessionsController < Devise::SessionsController
   def destroy
     @current_user = nil
     cookies.delete(:token)
-    respond_to_on_destroy
+    respond_to_destroy
   end
 
   # Need to overload this method to prevent it erroring every time
@@ -29,21 +30,14 @@ class Users::SessionsController < Devise::SessionsController
     @current_user.nil?
   end
 
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
-
   private
 
   def respond_to_create(user)
     render json: { user: }, status: :created
   end
 
-  def respond_to_on_destroy
-    render json: { success: 'User signed out.' }, status: :ok
+  def respond_to_destroy
+    render status: :no_content
   end
 
   def session_params
