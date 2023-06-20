90745d5e55c859f6ab75510333323c998580642d
kiizerd <ajsewell13@gmail.com>
2023-06-20 15:23:44 -0500

    Made rubocop-rspec happy.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,80 +1,103 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 # Register a new user
-RSpec.describe 'POST /users', type: :request do
-  context 'client submitted valid data' do
-    before { post '/users', params: { user: attributes_for(:user) } }
+RSpec.describe 'Users', type: :request do
+  describe 'POST /users' do
+    context 'with valid data' do
+      let!(:new_user_email) { 'register@here.com' }
 
-    it 'responds with the correct status(201) and new User' do
-      user = json_body[:user]
-      expect(response).to have_http_status(:created)
-      expect(user[:email]).to eq('foo@bar.com')
-    end
+      before do
+        post '/users', params: {
+          user: attributes_for(:user, email: new_user_email)
+        }
+      end
 
-    it 'sets token cookie' do
-      jar = get_cookie_jar(request, cookies)
-      expect(jar.encrypted[:token]).not_to be nil
-    end
-  end
-end
+      it 'responds with User' do
+        expect(json_body[:user][:email]).to eq(new_user_email)
+      end
 
-# Create a new session
-RSpec.describe 'POST /users/sign_in', type: :request do
-  before { create(:user) }
+      it 'responds with created(201) status' do
+        expect(response).to have_http_status(:created)
+      end
 
-  context 'client submitted valid data' do
-    before { post '/users/sign_in', params: { user: attributes_for(:user) } }
-
-    it 'responds with correct status(201) and User' do
-      expect(response).to have_http_status(:created)
-      expect(json_body[:user][:email]).to eq('foo@bar.com')
-    end
-
-    it 'sets token cookie' do
-      jar = get_cookie_jar(request, cookies)
-      expect(jar.encrypted[:token]).not_to be nil
+      it 'sets token cookie' do
+        jar = get_cookie_jar(request, cookies)
+        expect(jar.encrypted[:token]).not_to be nil
+      end
     end
   end
-end
 
-# Deletes an existing session
-RSpec.describe 'DELETE /users/sign_out', type: :request do
-  before { create(:user) }
+  # Create a new session
+  describe 'POST /users/sign_in' do
+    let!(:existing_user_email) { 'sign@in.com' }
 
-  context 'client is authorized' do
-    before do
-      post '/users/sign_in', params: { user: attributes_for(:user) }
-      delete '/users/sign_out'
+    before { create(:user, email: existing_user_email) }
+
+    context 'with valid data' do
+      before do
+        post '/users/sign_in', params: {
+          user: attributes_for(:user, email: existing_user_email)
+        }
+      end
+
+      it 'responds with User' do
+        expect(json_body[:user][:email]).to eq(existing_user_email)
+      end
+
+      it 'responds with created(201) status' do
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'sets token cookie' do
+        jar = get_cookie_jar(request, cookies)
+        expect(jar.encrypted[:token]).not_to be nil
+      end
     end
+  end
 
-    it 'responds with no-content(204) status code' do
-      expect(response).to have_http_status(:no_content)
+  # Deletes an existing session
+  describe 'DELETE /users/sign_out' do
+    context 'with authorized client' do
+      before do
+        authorize_test_session
+        delete '/users/sign_out'
+      end
+
+      it 'responds with no-content(204) status code' do
+        expect(response).to have_http_status(:no_content)
+      end
+
+      it 'removes token cookie' do
+        jar = get_cookie_jar(request, cookies)
+        expect(jar.encrypted[:token]).to be nil
+      end
     end
+  end
 
-    it 'removes token cookie' do
-      jar = get_cookie_jar(request, cookies)
-      expect(jar.encrypted[:token]).to be nil
+  # Deletes a User model
+  describe 'DELETE /users' do
+    context 'with authorized client' do
+      let(:email_to_delete) { 'good@bye.4ever' }
+
+      before do
+        post '/users', params: { user: attributes_for(:user, email: email_to_delete) }
+        delete '/users'
+      end
+
+      it 'deletes the authenticated User' do
+        expect(User.where(email: email_to_delete)).to eq []
+      end
+
+      it 'responds with no-content(204) status' do
+        expect(response).to have_http_status(:no_content)
+      end
+
+      it 'removes token cookie' do
+        jar = get_cookie_jar(request, cookies)
+        expect(jar.encrypted[:token]).to be nil
+      end
     end
-  end
-end
-
-# Deletes a User model
-RSpec.describe 'DELETE /users', type: :request do
-  before do
-    post '/users', params: { user: attributes_for(:user) }
-    delete '/users'
-  end
-
-  it 'deletes the authenticated User' do
-    expect(User.where(email: 'foo@bar.com')).to eq []
-  end
-
-  it 'responds with no content status' do
-    expect(response).to have_http_status(:no_content)
-  end
-
-  it 'removes token cookie' do
-    jar = get_cookie_jar(request, cookies)
-    expect(jar.encrypted[:token]).to be nil
   end
 end
