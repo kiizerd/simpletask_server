ed368e50c97e4352f2f4a9f3f1d92b175fccb51f
kiizerd <ajsewell13@gmail.com>
2023-06-20 20:39:23 -0500

    Added sequence for email and used in User factory.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,14 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+
   factory :user do
-    email { 'foo@bar.com' }
+    email { generate :email }
     password { 'password' }
   end
-end
 
 FactoryBot.define do
   factory :project do
