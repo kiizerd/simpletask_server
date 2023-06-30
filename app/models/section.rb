91355c90065d82638cb617be26719b477e5bbc01
kiizerd <ajsewell13@gmail.com>
2023-06-30 12:43:37 -0500

    Added correct section to user association.

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,6 @@
 class Section < ApplicationRecord
   belongs_to :project
+  has_one :user, through: :project
   has_many :tasks, -> { order(position: :desc) }, dependent: :destroy, inverse_of: :section
 
   default_scope -> { order(id: :asc) }
