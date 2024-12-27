c344cdf22413881a9420a5c7135b7573980d6a41
Alfred <68087676+kiizerd@users.noreply.github.com>
2024-12-26 18:29:56 -0600

    Update render-build.sh
    Matching render.com new guidelines for hosting Rails apps

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,4 +3,11 @@
 set -o errexit
 
 bundle install
-bundle exec rake db:migrate
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+
+# If you're using a Free instance type, you need to
+# perform database migrations in the build command.
+# Uncomment the following line:
+
+bundle exec rails db:migrate
